01d24590d6b392e557f1713a2cce1886541308ba
zft1371744864 <62921881+zft1371744864@users.noreply.github.com>
2021-01-04 13:58:08 +0800

    Update jdCookie.js

--- jdCookie.js
+++ jdCookie.js
@@ -4,7 +4,7 @@
 //此处填写京东账号cookie。
 //注：github action用户cookie填写到Settings-Secrets里面，新增JD_COOKIE，多个账号的cookie使用`&`隔开或者换行
 let CookieJDs = [
-  '',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJf0IUlADD0Xt1ufpZ0Zs8kIkv7hkSQQ9Xc_ubpKXrIpa9rI-kEJff9ZRojs-unnuE2v9TsCEQ;pt_pin=jd_5542080557927',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
   '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
 ]
 // 判断github action里面是否有京东ck
